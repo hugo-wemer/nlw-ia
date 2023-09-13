076c58a6a3068dfa17d6f65a4baf7b37a09234f1
Hugo Wemer <hugowemer@Hugos-MacBook-Air.local>
2023-09-13 17:39:11 -0300

    chore: create ai response route

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -2,11 +2,18 @@ import { fastify } from "fastify"
 import { getAllPromptsRoute } from "./routes/get-all-prompts"
 import { uploadVideoRoute } from "./routes/upload-video"
 import { createTranscriptionRoute } from "./routes/create-transcription"
+import { generateAICompletionsRoute } from "./routes/generate-ai-completion"
+import { fastifyCors } from "@fastify/cors"
+
 const app = fastify()
+app.register(fastifyCors, {
+  origin: '*', // qualquer URL frontend vai poder acessar meu backend
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionsRoute)
 
 
 
