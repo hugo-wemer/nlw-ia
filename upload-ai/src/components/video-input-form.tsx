db1e32323e7e8fed93a97efbf70116d4d1d4928f
Hugo Wemer <hugowemer@Hugos-MacBook-Air.local>
2023-09-14 00:01:08 -0300

    chore: communicate variables between front and backend

--- upload-ai/src/components/video-input-form.tsx
+++ upload-ai/src/components/video-input-form.tsx
@@ -9,9 +9,23 @@ import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from "@ffmpeg/util"
 import { api } from "@/lib/axios";
 
-export function VideoInputForm(){
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  uploading: 'Carregando...',
+  generating: 'Transcrevendo...',
+  success: 'Sucesso!'
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps){
   const [videoFile, setVideoFile] = useState<File | null>(null)
   const promptInputRef = useRef<HTMLTextAreaElement | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -32,6 +46,8 @@ export function VideoInputForm(){
     if(!videoFile){
       return
     }
+
+    setStatus("converting")
     // converte o vídeo em audio
     const audioFile = await converVideoToAudio(videoFile)
     // console.log(audioFile, prompt)
@@ -39,14 +55,18 @@ export function VideoInputForm(){
     const data = new FormData()
     data.append('file',  audioFile)
 
+    setStatus("uploading")
     const response = await api.post('/videos', data)
     const videoId = response.data.video.id
 
+    setStatus("generating")
     await api.post(`/videos/${videoId}/transcription`, {
       prompt,
     })
 
-    console.log('finalizou')
+    setStatus("success")
+
+    props.onVideoUploaded(videoId)
   }
 
   async function converVideoToAudio(video: File){
@@ -55,10 +75,6 @@ export function VideoInputForm(){
 
     // coloca um arquivo no contexto do ffmpeg
     await ffmpeg.writeFile('input.mp4', await fetchFile(video))
-
-    // ffmpeg.on('log', log => {
-    //   console.log(log)
-    // })
 
     ffmpeg.on('progress', progress => {
       console.log('Convert progress:' + Math.round(progress.progress * 100))
@@ -111,16 +127,24 @@ export function VideoInputForm(){
     <Separator />
     <div className="space-y-2">
       <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-      <Textarea 
+      <Textarea
+        disabled={status !== 'waiting'}
         ref={promptInputRef}
         id="transcription_prompt" 
         className="h-20 leading-relaxed resize-none"
         placeholder="Inclua palavras chave mencionadas no vídeo separadas por vírgula (,)"
         />
     </div>
-    <Button type="submit" className="w-full">
-      Carregar vídeo
-      <Upload className="w-4 h-4 ml-2"/>
+    <Button
+      data-success={status === 'success'}
+      disabled={status !== 'waiting'}
+      type="submit" 
+      className="w-full data-[success=true]:bg-emerald-400">
+      {status === 'waiting'? (
+        <>
+        Carregar vídeo
+        <Upload className="w-4 h-4 ml-2"/></>
+      ): statusMessages[status]}
     </Button>
   
   </form>
